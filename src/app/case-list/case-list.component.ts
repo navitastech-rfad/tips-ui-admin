194d1632fd5ef9d5108e7854bec1dc4e1f01c431
Tim Burns <tburns@hkandmassociates.com>
2018-07-30 20:47:06 -0400

    Added mock case list data
    Prasad:  I added mock-cases.ts and modified case-list.component.html and case-list.component.ts to use/display the mocked data.  I didn't get time to test it, but maybe you can pick up where I left off.

--- src/app/case-list/case-list.component.ts
+++ src/app/case-list/case-list.component.ts
@@ -1,74 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CaseListService } from '../_services/case-list.service';
+import { Case } from '../_models/case.model';
+import { CASES } from './mock-cases';
 // import { MatTableDataSource } from '@angular/material';
  // import { DataSource } from '@angular/cdk/table';
-
-export interface CaseList {
-  caseId: number;
-  firstName: string;
-  lastName: string;
-  address: string;
-  assigneeId: number;
-  closedStatus: boolean;
-}
-
-const CASE_DATA: CaseList[] = [
-  {
-    caseId: 10001,
-    firstName: 'Michael',
-    lastName: 'Jordan',
-    address: '101 Devon Street, Chicago IL 12334',
-    assigneeId: 101,
-    closedStatus: true
-  },
-  {
-    caseId: 10002,
-    firstName: 'LeBron',
-    lastName: ' James',
-    address: '101 Devon Street, Chicago IL 12334',
-    assigneeId: 101,
-    closedStatus: true
-  },
-  {
-    caseId: 10003,
-    firstName: 'Kevin',
-    lastName: '  Durant',
-    address: '101 Devon Street, Chicago IL 12334',
-    assigneeId: 102,
-    closedStatus: true
-  },
-  {
-    caseId: 10004,
-    firstName: 'Stephen',
-    lastName: '  Curry',
-    address: '101 Devon Street, Chicago IL 12334',
-    assigneeId: 102,
-    closedStatus: true
-  },
-  {
-    caseId: 10005,
-    firstName: 'Kobe',
-    lastName: '  Bryant',
-    address: '101 Devon Street, Chicago IL 12334',
-    assigneeId: 102,
-    closedStatus: true
-  },
-  {
-    caseId: 10006,
-    firstName: 'Chris',
-    lastName: '  Paul',
-    address: '101 Devon Street, Chicago IL 12334',
-    assigneeId: 103,
-    closedStatus: true
-  },
-  {
-    caseId: 10007,
-    firstName: 'Larry',
-    lastName: '  Bird',
-    address: '101 Devon Street, Chicago IL 12334',
-    assigneeId: 103,
-    closedStatus: true
-  }
-];
 
 
 @Component({
@@ -76,15 +11,25 @@ const CASE_DATA: CaseList[] = [
   templateUrl: './case-list.component.html',
   styleUrls: ['./case-list.component.css']
 })
-export class CaseListComponent  {
-
-  constructor() { }
-
-  displayedColumns: string[] = [ 'caseId', 'firstName', 'lastName', 'address', 'assigneeId'];
-  dataSource = CASE_DATA;
+export class CaseListComponent implements  OnInit  {
+  cases: Case[];  //<-Use the mock json data @Tim
+  
+  //private  cases:  Array<Case> = [];
+  constructor(private caseListService:  CaseListService) { }
 
   // applyFilter(filterValue: string) {
   //   this.dataSource.filter = filterValue.trim().toLowerCase();
   // }
+
+  ngOnInit() {
+    this.getCases();
+}
+
+  public  getCases() {
+    this.caseListService.getCases().subscribe((data:  Array<Case>) => {
+        this.cases  =  data;
+        console.log(data);
+    });
+}
 
 }
